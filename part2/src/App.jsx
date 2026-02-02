99b152d4a068c021460a2a21191748c5a526a29d
Sea25 <sonaabraham016@gmail.com>
2026-02-03 00:11:23 +0530

    Part2 created

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
+import React from 'react';
+import Course from './components/Course';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // Arbitrary number of courses (Exercise 2.4)
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        { id: 1, name: 'Fundamentals of React', exercises: 10 },
+        { id: 2, name: 'Using props to pass data', exercises: 7 },
+        { id: 3, name: 'State of a component', exercises: 14 },
+        { id: 4, name: 'Redux', exercises: 11 },
+      ],
+    },
+    {
+      id: 2,
+      name: 'Node.js',
+      parts: [
+        { id: 1, name: 'Routing', exercises: 3 },
+        { id: 2, name: 'Middlewares', exercises: 7 },
+      ],
+    },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
+    </div>
+  );
+};
 
-export default App
+export default App;
