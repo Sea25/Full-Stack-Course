ec17b3a4f2f8f055bcd651af126a353b8262c978
Sea25 <sonaabraham016@gmail.com>
2026-02-06 20:15:14 +0530

    3.1 DONE

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+
+const PORT = 3001
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456"
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523"
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345"
+  }
+]
+
+// route
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
