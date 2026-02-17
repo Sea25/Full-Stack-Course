d25574200e99cda2908ec057ab6c37f2b54a86ea
Sea25 <sonaabraham016@gmail.com>
2026-02-17 09:39:10 +0530

    2.3 done

--- part2/src/components/Course.jsx
+++ part2/src/components/Course.jsx
@@ -29,8 +29,17 @@ const Course = ({ course }) => {
     <div>
       <Header course={course} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
 
+
 export default Course
+
+// shows total exercises
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+
+  return <p><b>Total exercises: {total}</b></p>
+}
